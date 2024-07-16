6daa10ec6d322e9f29fede3a786ed4a164f68d3c
Mohamed waleed <Mohamed waleed@DESKTOP-0AUSUD8>
2024-07-16 12:52:52 +0300

    Add project files.

---
+++ F. Reversing/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n = int.Parse(Console.ReadLine());
+int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+Array.Reverse(arr);
+for (int i = 0; i < n; i++)
+{
+    Console.Write(arr[i] + " ");
+}
+Console.ReadLine();
